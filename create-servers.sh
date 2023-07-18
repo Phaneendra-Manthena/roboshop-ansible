894d94fc44fd959ffff3e2e9183282967995b955
phaneendra <phani.manthena27@gmail.com>
2023-07-18 13:07:07 +0530

    updating ./roboshop-ansible

--- create-servers.sh
+++ create-servers.sh
@@ -1,32 +1,10 @@
-#!/bin/bash
-
-# Function to create the virtual machine instance
-create_instance() {
-  gcloud compute instances create "$COMPONENT" \
-    --project "$PROJECT_ID" \
-    --zone "$ZONE" \
-    --machine-type "$MACHINE_TYPE" \
-    --image-family "$IMAGE_FAMILY" \
-    --image-project "$IMAGE_PROJECT"
-
-  if [ $? -eq 0 ]; then
-    echo "Server Created - SUCCESS - Instance Name: ${COMPONENT}"
-  else
-    echo "Server Created - FAILED - Instance Name: ${COMPONENT}"
-    exit 1
-  fi
-}
-
-# Variables (customize as needed)
-PROJECT_ID="phani-389308"
-ZONE="us-west1-b"
-MACHINE_TYPE="e2-medium"
-IMAGE_FAMILY="centos-8"
-IMAGE_PROJECT="centos8"
-env="dev"
-
-# Create the virtual machine instances
-for component in catalogue1 cart1 user1 shipping1 payment1 frontend1 mongodb1 mysql1 rabbitmq1 redis1; do
-  COMPONENT="${component}-${env}"
-  create_instance
-done
+git pull ; ansible-playbook -i hosts roboshop.yml -e ROLE_NAME=mysql -e server=mysql
+git pull ; ansible-playbook -i hosts roboshop.yml -e ROLE_NAME=redis -e server=redis
+git pull ; ansible-playbook -i hosts roboshop.yml -e ROLE_NAME=rabbitmq -e server=rabbitmq
+git pull ; ansible-playbook -i hosts roboshop.yml -e ROLE_NAME=mongodb -e server=mongodb
+git pull ; ansible-playbook -i hosts roboshop.yml -e ROLE_NAME=user -e server=user -e env=dev
+git pull ; ansible-playbook -i hosts roboshop.yml -e ROLE_NAME=cart -e server=cart -e env=dev
+git pull ; ansible-playbook -i hosts roboshop.yml -e ROLE_NAME=catalogue -e server=catalogue -e env=dev
+git pull ; ansible-playbook -i hosts roboshop.yml -e ROLE_NAME=shipping -e server=shipping -e env=dev
+git pull ; ansible-playbook -i hosts roboshop.yml -e ROLE_NAME=payment -e server=payment -e env=dev
+git pull ; ansible-playbook -i hosts roboshop.yml -e ROLE_NAME=frontend -e server=frontend -e env=dev
