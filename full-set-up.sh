306f4149ff6745d7e2264a68106749d66d7967bd
phaneendra <phani.manthena27@gmail.com>
2023-07-24 17:57:07 +0530

    updating ./roboshop-ansible

--- full-set-up.sh
+++ full-set-up.sh
@@ -1,7 +1,4 @@
-#for component in frontend cart catalogue user shipping payment redis mongodb mysql rabbitmq ; do
-#ansible-playbook -i hosts roboshop.yml -e ROLE_NAME=$component -e server= -e env=dev
-#
-#done
-for component in frontend cart catalogue user shipping payment redis mongodb mysql rabbitmq; do
-    ansible-playbook -i hosts roboshop.yml -e ROLE_NAME=$component -e "server=$(grep -oP "${component}\s+\K\S+" hosts | tr '\n' ',')" -e env=dev
+for component in frontend cart catalogue user shipping payment redis mongodb mysql rabbitmq ; do
+ansible-playbook -i hosts roboshop.yml -e ROLE_NAME=$component -e server=$1 -e env=dev
+
 done
